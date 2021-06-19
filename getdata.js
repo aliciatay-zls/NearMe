011017e612764075ac6023a5a7a8b8cf0afc030e
aliciatay-zls <e0424736@u.nus.edu>
2021-06-19 16:27:03 +0800

    Mentor's To Do list

---
+++ getdata.js
@@ -0,0 +1,7 @@
+
+
+// Connect to MySQL
+// Execute SQL statements (or copy paste here, use lib)
+// Retrieve all records as a 2D array
+
+// Iterate the 2D array and console.log
---
+++ rest_api.js
@@ -0,0 +1,7 @@
+// Start a NodeJS web app
+// Create a new Route http://localhost:3000/outlets
+// Return the 2D array outlets as JSON
+
+// Pass in currentLatitude and currentLongitude as HTTP GET:
+// GET http://localhost:3000/outlets?currentLatitude=1.3104680812609208&currentLongitude=103.86246226812166
+// Use currentLatitude and currentLongitude in the SQL statement
