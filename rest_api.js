8d5ccdcb791bd969a74cdda16e6ed720addfec2e
aliciatay-zls <e0424736@u.nus.edu>
2021-07-10 15:45:04 +0800

    Use db-config.js to configure knex in rest_api.js

--- rest_api.js
+++ rest_api.js
@@ -4,28 +4,18 @@
 // Pass in currentLatitude and currentLongitude as HTTP GET:
 // GET http://localhost:3000/outlets?currentLatitude=1.3104680812609208&currentLongitude=103.86246226812166
 // Use currentLatitude and currentLongitude in the SQL statement
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.static('html')); //dir name
 
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      host : process.env.DB_HOST,
-      database : process.env.DB_NAME,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD
-    }
-});
+const db = require("./db-config.js");
 
 // Create a new Route http://nearme.aliciatay.com/outlets
 // Return the 2D array outlets as JSON
 app.get('/outlets', (req, res) => {
     const results = {
-        sectionTitle: req.query.brand,
         outlets: [],
         currentLocation: {
             latitude: parseFloat(req.query.currentLatitude),
@@ -37,9 +27,9 @@ app.get('/outlets', (req, res) => {
 
     //retrieves the brand name for the results that will follow
     if (req.query.brand.length > 0) {
-        knex('brands')
+        db('brands')
         .where('ShortName', req.query.brand)
-        .select('BrandId')
+        .select('BrandId', 'BrandName')
         .then(function(rows) {
             if (rows.length == 0) {
                 console.log("Could not find brand in DB.");
@@ -47,6 +37,7 @@ app.get('/outlets', (req, res) => {
                 return;
             }
             selectedBrandId = rows[0]['BrandId'];
+            results.sectionTitle = rows[0]['BrandName'];
     
             //retrieves details of outlets sorted by increasing distance from user's current position
             //then sends everything as JSON
@@ -56,7 +47,7 @@ app.get('/outlets', (req, res) => {
             const part4 = "ORDER BY distance ASC"
             const fullQuery = part1 + part2 + part3 + part4;
     
-            knex.raw(fullQuery, [results.currentLocation.latitude, results.currentLocation.longitude, selectedBrandId])
+            db.raw(fullQuery, [results.currentLocation.latitude, results.currentLocation.longitude, selectedBrandId])
                 .then(function(rows) {
                     rows[0].forEach((row) => {
                         results.outlets.push({ 
@@ -70,7 +61,7 @@ app.get('/outlets', (req, res) => {
                     res.send(results);
                 })
                 .catch((error) => console.error(error));
-                // .finally(() => knex.destroy());
+                // .finally(() => db.destroy());
         });
     }
 
