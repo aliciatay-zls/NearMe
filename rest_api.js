2e26ed56ecedd98afa7984d6a9b00cc61345c246
aliciatay-zls <e0424736@u.nus.edu>
2021-07-10 16:44:37 +0800

    Use knex transactions in rest_api.js with mentor

--- rest_api.js
+++ rest_api.js
@@ -14,7 +14,7 @@ const db = require("./db-config.js");
 
 // Create a new Route http://nearme.aliciatay.com/outlets
 // Return the 2D array outlets as JSON
-app.get('/outlets', (req, res) => {
+app.get('/outlets', async (req, res) => {
     const results = {
         outlets: [],
         currentLocation: {
@@ -23,46 +23,57 @@ app.get('/outlets', (req, res) => {
         }
     };
 
-    let selectedBrandId = null;
+    let selectedBrands = null;
 
     //retrieves the brand name for the results that will follow
     if (req.query.brand.length > 0) {
-        db('brands')
-        .where('ShortName', req.query.brand)
-        .select('BrandId', 'BrandName')
-        .then(function(rows) {
-            if (rows.length == 0) {
+        await db.transaction(async trx => {
+            console.log("Find Brand ID for:", req.query.brand)
+
+            const brands = await db('brands')
+                .where('ShortName', req.query.brand)
+                .select('BrandId', 'BrandName')
+                .transacting(trx);
+
+            if (brands.length === 0) {
                 console.log("Could not find brand in DB.");
                 res.send(results);
                 return;
             }
-            selectedBrandId = rows[0]['BrandId'];
-            results.sectionTitle = rows[0]['BrandName'];
-    
-            //retrieves details of outlets sorted by increasing distance from user's current position
-            //then sends everything as JSON
-            const part1 = "SELECT o.OutletId, o.OutletName, o.Latitude, o.Longitude, o.Postal, o.Contact, o.Closing, b.BrandId, b.BrandName, ";
-            const part2 = "DISTANCE(?, ?, Latitude, Longitude, 'KM' ) AS distance FROM outlets o INNER JOIN brands b USING(BrandId) ";
-            const part3 = "WHERE o.BrandId = ? "
-            const part4 = "ORDER BY distance ASC"
-            const fullQuery = part1 + part2 + part3 + part4;
-    
-            db.raw(fullQuery, [results.currentLocation.latitude, results.currentLocation.longitude, selectedBrandId])
-                .then(function(rows) {
-                    rows[0].forEach((row) => {
-                        results.outlets.push({ 
-                            name: row['OutletName'], 
-                            distance: row['distance'], 
-                            postal: row['Postal'], 
-                            contact: row['Contact'], 
-                            closing: row['Closing']
-                        });
-                    });
-                    res.send(results);
-                })
-                .catch((error) => console.error(error));
-                // .finally(() => db.destroy());
-        });
+      
+            console.log('Brands', brands)
+            selectedBrands = [ brands[0]['BrandId'] ];
+            results.sectionTitle = brands[0]['BrandName'];
+
+            const queryParts = [];
+            queryParts.push("SELECT o.OutletName, o.Latitude, o.Longitude, o.Postal, o.Contact, o.Closing, b.ShortName, DISTANCE(?, ?, Latitude, Longitude, 'KM' ) AS distance");
+            queryParts.push("FROM outlets o INNER JOIN brands b USING(BrandId)");
+            queryParts.push("WHERE o.BrandId IN (?)");
+            queryParts.push("ORDER BY distance ASC");
+
+            const queryParams = [results.currentLocation.latitude, results.currentLocation.longitude, selectedBrands];
+
+            const outlets = await db.raw(queryParts.join(" "), queryParams)
+                .transacting(trx);
+                
+            if (outlets[0].length === 0) {
+                console.log("Could not find outlets in DB.");
+                res.send(results);
+                return;
+            }
+
+            outlets[0].forEach((outlet) => {
+                results.outlets.push({ 
+                    name: outlet['OutletName'], 
+                    brandShortName: outlet['ShortName'],
+                    distance: outlet['distance'], 
+                    postal: outlet['Postal'], 
+                    contact: outlet['Contact'], 
+                    closing: outlet['Closing']
+                });
+            });
+            res.send(results);
+        })
     }
 
     if (req.query.category.length > 0) {
