cb605722290a927961b6aa32c5c3c1601033a804
aliciatay-zls <e0424736@u.nus.edu>
2021-07-31 16:22:57 +0800

    Move parser to its own class
    - refactor parser_kfc.js

--- parser_kfc.js
+++ parser_kfc.js
@@ -1,130 +1,79 @@
-const fetch = require("node-fetch");
+const Parser = require("./parser");
 const jsdom = require("jsdom");
-const dbManager = require("./db_manager.js");
 
+class KFCParser extends Parser {
+    static get defaultURL() {
+        return "https://www.kfc.com.sg/Location/Search";
+    }
 
-// Generates the brand name and its short form for parsing.
-function getBrandDetails(url, shortName) {
-    return new Promise((resolve, reject) => {
-        try{
-            const urlObj = new URL(url);
-            var fullName = urlObj.hostname.replace("www", '').replace("com", '').replace("sg", '').replace(/\./g, '').trim();
-            fullName = fullName.toUpperCase();
-            const successObj = {
-                msg: "Success",
-                data: [
-                {brandName: fullName,
-                shortName: shortName}
-                ]
-            };
-            resolve(successObj);
-        } catch(err) {
-            const failureObj = {
-                msg: err.message
-            };
-            reject(failureObj);
+    static get defaultBrandDetails() {
+        return {
+            BrandName: "KFC",
+            ShortName: "kfc",
+            Keywords: "kfc, restaurant, fried chicken, finger lickin good, chicken, fast food"
         }
-    });
-}
+    }
 
+    constructor() {
+        super(KFCParser.defaultURL, KFCParser.defaultBrandDetails)
+    }
 
-// Requests the page, checks the response header and collects raw html
-// before converting it into a DOM to be parsed.
-function getData(url, brandDetails) {
-    return fetch(url, {
-    "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-language": "en-US,en;q=0.9,de;q=0.8",
-        "cache-control": "max-age=0",
-        "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "cookie": "_gcl_au=1.1.973224319.1623480306; _ga=GA1.3.261762845.1623480306; __qca=P0-746513972-1623480306139; KFCSG.A.SID=3fsr5fonvrdjgjguutyhk4oo; KFCSG.ReMe=False; KFCSG.IMS=False; KFCSG.A.SID_n=3fsr5fonvrdjgjguutyhk4oo; _gid=GA1.3.1667014086.1627623594; _gat_UA-9403339-2=1; AWSALB=VfYsv8Xp6yv8CljvrqozCBRpksr+gZmqMUbM6To4pv9AQV4bP2cqK9YBXv/Ppyv/plLhSehswM+uwA1EM6GLcckSHZVW9vGXUaibMP6PjXfrqX8jwI1Af9B4fq7v; AWSALBCORS=VfYsv8Xp6yv8CljvrqozCBRpksr+gZmqMUbM6To4pv9AQV4bP2cqK9YBXv/Ppyv/plLhSehswM+uwA1EM6GLcckSHZVW9vGXUaibMP6PjXfrqX8jwI1Af9B4fq7v"
-    },
-    "referrerPolicy": "strict-origin-when-cross-origin",
-    "body": null,
-    "method": "GET",
-    "mode": "cors"
-    })
-    .then(res => res.text())
-    .then(rawHtml => {
+    getRows(rawHtml) {
         try {
             const dom = new jsdom.JSDOM(rawHtml);
-            return parseForLatLong(dom, brandDetails);
+            const allOutlets = dom.window.document.querySelectorAll("div.restaurantDetails");
+            
+            const data = [];
+            for (let outlet of allOutlets) {
+                data.push(this.parseRow(outlet));
+            }
+
+            return data;
         } catch (err) {
             console.error(err.message);
         }
-    });
-}
+    }  
 
-
-// Parses the DOM object for the outlet details for KFC,
-// then writes the collected data into locationsDB.
-function parseForLatLong(domObj, brandDetails) {
-    const data = [];
-    const brandName = brandDetails[0].brandName.concat(" ");
-
-    const allOutlets = domObj.window.document.querySelectorAll("div.restaurantDetails");
-    for (let outlet of allOutlets) {
-        let entry = {};
-
-        let name = outlet.getAttribute("data-restaurantname").trim();
+    // All these functions are not async because they do not use any async calls
+    getOutletName(outletNode) {
+        let name = outletNode.getAttribute("data-restaurantname").trim();
         if (name.length == 0) {
-            console.log("Entry removed. Outlet name unknown.");
-            continue;
+            throw Error("Entry removed. Outlet name unknown.");
         }
-        entry.OutletName = brandName.concat(name);
+        return `${this.brandDetails.BrandName} ${name}`;
+    }
 
-        let latitude = outlet.getAttribute("data-latitude").trim();
+    getLatitude(outletNode) {
+        let latitude = outletNode.getAttribute("data-latitude").trim();
         latitude = parseFloat(latitude);
         if (isNaN(latitude)) {
-            console.log(`Entry for "${entry.OutletName}" removed. Latitude unknown/invalid.`);
-            continue;
+            throw Error(`Entry for "${entry.OutletName}" removed. Latitude unknown/invalid.`);
         }
-        entry.Latitude = latitude;
+        return latitude;
+    }
 
-        let longitude = outlet.getAttribute("data-longitude").trim();
+    getLongitude(outletNode) {
+        let longitude = outletNode.getAttribute("data-longitude").trim();
         longitude = parseFloat(longitude);
         if (isNaN(longitude)) {
-            console.log(`Entry for "${entry.OutletName}" removed. Longitude unknown/invalid.`);
-            continue;
+            throw Error(`Entry for "${entry.OutletName}" removed. Longitude unknown/invalid.`);
         }
-        entry.Longitude = longitude;
-
-        let postalCode = outlet.getAttribute("data-address-pincode").trim();
-        entry.Postal = postalCode.replace(/[^0-9]/g, '');
-
-        let contactNum = outlet.getAttribute("data-phoneno").trim();
-        entry.Contact = contactNum.replace(/\s/g, '');
-
-        entry.Closing = outlet.getAttribute("data-timing").trim();
-
-        data.push(entry);
+        return longitude;
     }
-    return Promise.resolve({
-        outlets: data, 
-        brand: brandDetails
-    });
+
+    getPostal(outletNode) {
+        let postalCode = outletNode.getAttribute("data-address-pincode").trim();
+        return postalCode.replace(/[^0-9]/g, '');
+    }
+
+    getContact(outletNode) {
+        let contactNum = outletNode.getAttribute("data-phoneno").trim();
+        return contactNum.replace(/\s/g, '');
+    }
+
+    getClosing(outletNode) {
+        return outletNode.getAttribute("data-timing").trim();
+    }
 }
 
-
-// Runner function.
-function parseKfc() {
-    const url = "https://www.kfc.com.sg/Location/Search";
-    const shortName = "kfc";
-    const keywordsList = "kfc, restaurant, fried chicken, finger lickin good, chicken, fast food";
-    getBrandDetails(url, shortName)
-    .then((results) => getData(url, results.data))
-    .then(async (results) => {
-        await dbManager.writeOutletsToDb(results.outlets, results.brand);
-        await dbManager.updateKeywordsInDb(keywordsList, shortName);
-        return Promise.resolve("Parsed KFC");
-    })
-    .catch((err) => console.error("Error:", err.message));
-}
-
-module.exports = parseKfc;
+module.exports = KFCParser;
--- run_parsers.js
+++ run_parsers.js
@@ -1,16 +1,15 @@
-const parseKfc = require("./parser_kfc.js");
-const parseMcd = require("./parser_mcd.js");
-const parseNfp = require("./parser_nfp.js");
+const KFCParser = require("./parser_kfc.js");
+// const parseMcd = require("./parser_mcd.js");
+// const parseNfp = require("./parser_nfp.js");
 
-// To-do: maybe wrap these in a function too, call before run().
-const parsers = [];
-parsers.push(parseKfc, parseMcd, parseNfp);
+const parsers = [
+    new KFCParser()
+];
 
-// Problem: BrandId still inserted in wrong order (whichever finishes first).
-// dbManager.writeOutletsToDb() in each parser? Wrong use of promises?
 async function run() {
     for (const parser of parsers) {
-        await parser();
+        const outlets = await parser.fetchData();
+        await parser.saveData(outlets);
     }
 }
 
