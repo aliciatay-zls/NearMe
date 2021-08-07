eb4681dc5f885cbe38a7342cec2317aeb189158c
aliciatay-zls <e0424736@u.nus.edu>
2021-08-07 15:45:20 +0800

    Attach app to db

--- knexfile.js
+++ knexfile.js
@@ -33,12 +33,7 @@ module.exports = {
 
   production: {
     client: 'mysql2',
-    connection: {
-      host : process.env.DB_HOST,
-      database : process.env.DB_NAME,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD
-    },
+    connection: process.env.CLEARDB_DATABASE_URL,
     migrations: {
       directory : "./data/migrations"
     }
