2d21d253d236305773b1869ead5d73fc916effcb
aliciatay-zls <e0424736@u.nus.edu>
2021-08-28 15:50:43 +0800

    Fix display of search results, messages to user with mentor

--- html/locations.js
+++ html/locations.js
@@ -6,18 +6,17 @@ let isGetLocationSuccessful = false;
 
 // Messages to be displayed to the user on the webpage
 const locationHelpMessage = `
-  We're sorry! NearMe was unable to determine your location. This could be due to the following reasons:\n
+  We're sorry! NearMe was unable to determine your location. This could be due to the following reasons:<br>
   1) NearMe does not have permission to get your location.
-  \t- Allow location services for NearMe in your browser. Then, refresh the page.\n
+  \t- Allow location services for NearMe in your browser. Then, refresh the page.<br>
   2) NearMe has permission, but is unable to get your correct/precise location.
-  \t- In your browser's settings, ensure sites are allowed to ask for your location.\n
+  \t- In your browser's settings, ensure sites are allowed to ask for your location.<br>
   3) Network connectivity issues :(
-  \t- Please try again later.\n
+  \t- Please try again later.<br>
   Refer to the NearMe guide for more information.
 `;
 const noSearchWordMessage = "Enter something to get started.";
-const noRadiusMessage = "No distance limit selected. Showing all outlets found.";
-
+const noRadiusMessage = "<strong>No distance limit selected. Showing all outlets found.</strong><br></br>";
 
 // Retrieves the latitude and longitude of the user's current location
 async function geoGetCurrentLocation() {
@@ -46,8 +45,13 @@ $(document).ready(async function() {
   console.log("Successfully got location:", isGetLocationSuccessful);
   console.log("Lat, long:", currentLatitude, currentLongitude);
   if (!isGetLocationSuccessful) {
-    $(`<div class='is-error'>${locationHelpMessage}</div>`)
-    .appendTo(".messages");
+    var template = Handlebars.compile($(".message-template").html());
+    var results = template({
+      messageToUser: {
+        message: locationHelpMessage
+      }
+    });
+    $(".messages").append(results);
     $("fieldset").attr("disabled", true);
   }
 
@@ -82,32 +86,39 @@ $(document).ready(async function() {
     if (params.searchWord.length == 0) {
       $("#searchWord").addClass("is-empty");
       $(`<div class='help-block'>${noSearchWordMessage}</div>`)
-      .appendTo("#search-bar");
+      .appendTo("#search-bar"); //TO-FIX
       return;
     }
 
     if (isNaN(parseFloat(params.radius))) {
-      $(".messages").append(`<b>${noRadiusMessage}</b><br></br>`);
+      var template = Handlebars.compile($(".message-template").html());
+      var results = template({
+        messageToUser: {
+          message: noRadiusMessage
+        }
+      });
+      $(".messages").append(results);
     }
     
     const url = "/outlets";
     $.getJSON(url, params, function(data) {
-      Handlebars.registerHelper('isTopThreeResult', function (index) {
-        return (index < 3);
-      });
-      Handlebars.registerHelper('isTopResult', function (index) {
-        return (index == 0);
-      });
-      Handlebars.registerHelper('isStartOfRemainingResults', function(index) {
-        return (index == 3);
+      Handlebars.registerHelper('hasRemainingOutlets', function(data) {
+        return (data.length > 0);
       })
 
+      let topThree = data.outlets, remainingOutlets = [];
+      if (data.outlets.length > 3) {
+        topThree = data.outlets.slice(0, 3);
+        remainingOutlets = data.outlets.slice(3);
+      }
+
+      let outletsToDisplay = {topThree: topThree, remainingOutlets: remainingOutlets};
+      console.log('outletsToDisplay:', outletsToDisplay)
       var template = Handlebars.compile($('#result-row').html());
-      var results = template(data);
+      var results = template(outletsToDisplay);
 
       $("#results-description").append(data.messageToUser);
       $("#outletResults").append(results);
-      $("</div>").appendTo("#outletResults");
 
       $("#search-page").addClass("is-hidden");
       $("#results-page").removeClass("is-hidden");
